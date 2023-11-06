d9f5d1d3e146511227278bffc6db4d803bf80ff2
Tarunjit <113191713+Tarun-1907@users.noreply.github.com>
2023-11-06 15:55:32 +0530

    Add files via upload

---
+++ indx.py
@@ -0,0 +1,19 @@
+course="'Hello' Everyone"
+print(course)
+course='Hi "Bye"'
+print(course)
+course='''
+Hi Tarun 
+nice to meet you 
+'''
+print(course)
+course='python for beginners'
+print(course[0])
+print(course[-1])
+print(course[0:3])
+print(course[0:])
+print(course[:5])
+another=course[:]
+print(another)
+name='Hello'
+print(name[1:-1])
