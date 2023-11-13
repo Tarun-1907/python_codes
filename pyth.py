10e5f7aa79d0dcdccb4cc657b5c65cf8fe86c612
Tarunjit <113191713+Tarun-1907@users.noreply.github.com>
2023-11-14 00:40:57 +0530

    Add files via upload

---
+++ pyth.py
@@ -0,0 +1,7 @@
+import turtle
+graphics=turtle.Turtle()
+wn=turtle.Screen()
+wn.bgpic("download.gif") # It will changes the background picture
+# bgcolor() for changing the background color
+# title() For set the title of the document
+# bgpic() for changing the background picture
