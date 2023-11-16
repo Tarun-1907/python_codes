15085d4dc1e163a0700c438639cc7fd016251433
Tarunjit <113191713+Tarun-1907@users.noreply.github.com>
2023-11-16 19:41:11 +0530

    Add files via upload

---
+++ for.py
@@ -0,0 +1,10 @@
+for item in 'python': 
+    print(item)
+for item in [1,2,3,4,5]:
+    print(item) 
+for item in range(10):
+    print(item)
+for item in range(5,10):
+    print(item)
+for item in range(5,10,2):
+    print(item)
