18d195b5f7a5316ea0adaf54540a967ee9e8611d
Tarunjit <113191713+Tarun-1907@users.noreply.github.com>
2023-11-17 22:25:38 +0530

    Add files via upload

---
+++ funct.py
@@ -0,0 +1,9 @@
+course='python for beginners'
+print(len(course))
+print(course.upper())
+print(course.lower())
+print(course)
+print(course.find('P'))
+print(course.replace('beginners','Absolute beginners'))
+print('python' in course)
+print(course.title())
