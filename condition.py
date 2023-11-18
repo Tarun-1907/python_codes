505004adaf21b095dd0b813401187cf079d6bc93
Tarunjit <113191713+Tarun-1907@users.noreply.github.com>
2023-11-18 14:48:21 +0530

    Add files via upload

---
+++ condition.py
@@ -0,0 +1,7 @@
+has_high_income=True
+good=True
+if has_high_income or good:                 # for and both condition is True
+    print("Eligible for loan")              # for or one condition should be True
+else:                                       # for and not condition should be false
+    print("Get lost")    
+    
