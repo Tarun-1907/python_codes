0fea2a622716a68fc18bf0a94907b34c752c7883
Tarunjit <113191713+Tarun-1907@users.noreply.github.com>
2023-12-01 19:47:02 +0530

    Add files via upload

---
+++ ifel.py
@@ -0,0 +1,11 @@
+is_hot=False
+is_cold=False
+if is_hot:
+    print("It is a very hot day")
+    print("Drink plenty of water")
+elif is_cold:
+    print("It is a very cold day")
+    print("Wear warm clothes")
+else:
+    print("It is a very good day")
+print("Enjoy your day")          
