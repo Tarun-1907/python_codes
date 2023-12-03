0190cd84656d2ab366751c64fc547d7c322a48eb
Tarunjit <113191713+Tarun-1907@users.noreply.github.com>
2023-12-03 17:47:34 +0530

    Add files via upload

---
+++ pattrn.py
@@ -0,0 +1,10 @@
+for i in range(4):
+   for i in range(4):
+     print('# ',end=" ")
+   print()
+   
+for i in range(4):
+     for j in range(4-i):
+         print('# ',end="")
+     print()
+     
