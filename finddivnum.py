1c76a22852b5c8627eb17ae80b18dce236dfc83f
Tarunjit <113191713+Tarun-1907@users.noreply.github.com>
2023-12-07 12:23:43 +0530

    Add files via upload

---
+++ finddivnum.py
@@ -0,0 +1,10 @@
+nums=[4,19,26,15,49,30]
+for num in nums:
+    if num%5==0:
+        print(f"[{num} is divisable by 5]")
+     #  print(f"{num} / 5 ./")
+     #  break
+    else:
+      print(f"{num} is not divisable by 5")
+     # print(f"{num} / 5  X")
+     
